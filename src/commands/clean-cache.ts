94af4bca31be1b5c4111633dccb116af242c731e
Dennise Catolos <dentolos19@gmail.com>
2026-03-01 16:59:59 +0800

    Formatted source code

--- src/commands/clean-cache.ts
+++ src/commands/clean-cache.ts
@@ -8,9 +8,7 @@ export default function cleanCache(env: Environment) {
       fs.rmSync(env.wikiStorePath, { recursive: true });
       vscode.window.showInformationMessage("Cache cleaned successfully!");
     } catch {
-      vscode.window.showErrorMessage(
-        "Failed to clean cache! Try to close any open wiki workspaces."
-      );
+      vscode.window.showErrorMessage("Failed to clean cache! Try to close any open wiki workspaces.");
     }
   } else {
     vscode.window.showInformationMessage("Cache is already clean!");
